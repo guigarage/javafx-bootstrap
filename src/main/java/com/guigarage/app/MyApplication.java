86810b9e20063135bcf8e79dc253277a8ce4d5aa
Hendrik Ebbers <hendrik.ebbers@web.de>
2016-08-10 13:52:13 +0200

    minor changes

--- src/main/java/com/guigarage/app/MyApplication.java
+++ src/main/java/com/guigarage/app/MyApplication.java
@@ -1,6 +1,7 @@
 package com.guigarage.app;
 
 import javafx.application.Application;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
@@ -11,13 +12,21 @@ public class MyApplication extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
 
-        Scene scene = new Scene(new StackPane(new Label("Hello World!")));
-        scene.getStylesheets().add(MyApplication.class.getResource("style.css").toExternalForm());
+        Scene scene = new Scene(getRootNode());
+        addStylesheet(scene);
 
         primaryStage.setScene(scene);
         primaryStage.setWidth(800);
         primaryStage.setHeight(600);
         primaryStage.show();
+    }
+
+    private void addStylesheet(Scene scene) {
+        scene.getStylesheets().add(MyApplication.class.getResource("style.css").toExternalForm());
+    }
+
+    private Parent getRootNode() {
+        return new StackPane(new Label("Hello World!"));
     }
 
     public static void main(String... args) {
--- src/main/java/com/guigarage/app/mainview/MainViewController.java
+++ src/main/java/com/guigarage/app/mainview/MainViewController.java
@@ -1,7 +1,4 @@
 package com.guigarage.app.mainview;
 
-/**
- * Created by hendrikebbers on 10.05.16.
- */
 public class MainViewController {
 }
